1168a7424a8fee00c109187f9ff2a423a32a36af
odychance <odychance96@gmail.com>
2023-05-07 15:35:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState, useEffect} from 'react';
+import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
+import Info from './components/Info';
+
+import axios from 'axios'
 
 function App() {
+
+  const [busquedaLetra, setBusquedaLetra] = useState({})
+  const [letra, setLetra] = useState('')
+  const [info, setInfo] = useState({})
+
+  useEffect(() => {
+    if(Object.keys(busquedaLetra).length === 0 ) return
+
+    const consultarLetraAPI = async () => {
+
+      const {artista, cancion} = busquedaLetra
+
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`
+      const url2 = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${artista}`
+
+      const [letra, informacion] = await Promise.all([
+        axios(url),
+        axios(url2)
+      ])
+
+        setLetra(letra.data.lyrics)
+        setInfo(informacion.data.artists[0])
+
+      // setLetra(resultado.data.lyrics)
+    }
+    consultarLetraAPI()
+  }, [busquedaLetra, info])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Formulario
+        setBusquedaLetra={setBusquedaLetra}
+      />
+
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <Info 
+              info={info}
+            />
+          </div>
+          <div className='col-md-6'>
+            <Cancion
+              letra={letra}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
